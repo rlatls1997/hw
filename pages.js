65ab736f1f7f3b1c4ef340aad2e53604b608aaea
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-03-25 18:18:13 +0900

    first commit

---
+++ pages.js
@@ -0,0 +1,6 @@
+let studentCount = 31;
+let pageSize = 10;
+
+let pageNum = Math.ceil(studentCount/pageSize);
+
+console.log(pageNum);
---
+++ random.js
@@ -0,0 +1,3 @@
+for(let i = 0; i<30; i++){
+    console.log(Math.round(Math.random()*10+10))
+}
