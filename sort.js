e594019659236b272d45958e2d6b255044968944
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-04-01 19:21:42 +0900

    first commit

--- sort.js
+++ sort.js
@@ -3,6 +3,6 @@ let a = []
 for(let i = 0; i<100; i++){
     a[i] = Math.floor(Math.random()*100+1);
 }
-a.sort(function(a,b){return a-b})   //
+a.sort(function(a,b){return a-b})   
 
 console.log(a)
