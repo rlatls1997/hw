05ae579a93ceb6b215fdfe246c8b8a9756a32ba3
Kim, Sinhyuk <rlatls1997@gmail.com>
2020-04-09 03:51:21 +0900

    first commit

---
+++ timer1.js
@@ -0,0 +1,13 @@
+let count = 0;
+
+function printTime(msg){
+    console.log(msg, new Date());
+    count++;
+    if(count == 10){
+        clearInterval(id);
+    }
+}
+
+let id = setInterval(printTime,1000,"1초간격");
+
+
